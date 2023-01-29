5e45752156c665c372e9112dbabc086eeac9221d
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2023-01-29 23:33:03 +0200

    add some fix of data favicon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,53 @@
-import React, {useEffect, useState} from 'react';
-import Style from './App.module.scss'
-import {Route, Routes} from "react-router-dom";
-import {Box, Grid} from "@mui/material";
-import Navigation from './Navigation/Navigation';
+import React, { useEffect, useState } from "react";
+import Style from "./App.module.scss";
+import { Route, Routes } from "react-router-dom";
+import { Box, Grid } from "@mui/material";
+import Navigation from "./Navigation/Navigation";
 
-import ProjectsPage from '../pages/ProjectsPage/ProjectsPage'
-import About from './About/About';
-import Home from './Home/Home';
+import ProjectsPage from "../pages/ProjectsPage/ProjectsPage";
+import About from "./About/About";
+import Home from "./Home/Home";
 
 export const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
-   function handleToggleDarkMode() {
-      let oppositeOfCurrentDarkMode = !darkMode
-      localStorage.setItem('darkMode', `${oppositeOfCurrentDarkMode}`)
-      setDarkMode(oppositeOfCurrentDarkMode)
-   }
+  function handleToggleDarkMode() {
+    let oppositeOfCurrentDarkMode = !darkMode;
+    localStorage.setItem("darkMode", `${oppositeOfCurrentDarkMode}`);
+    setDarkMode(oppositeOfCurrentDarkMode);
+  }
 
-   useEffect(() => {
-      // eslint-disable-next-line
-      let detectedDarkMode = eval(localStorage.getItem('darkMode')); 
+  useEffect(() => {
+    // eslint-disable-next-line
+    let detectedDarkMode = eval(localStorage.getItem("darkMode"));
 
-      if (detectedDarkMode) {
-         setDarkMode(detectedDarkMode)
-      } else {
-         localStorage.setItem('darkMode', 'false')
-      }
-   }, [])
+    if (detectedDarkMode) {
+      setDarkMode(detectedDarkMode);
+    } else {
+      localStorage.setItem("darkMode", "false");
+    }
+  }, []);
 
-    return (
-          <Box className={darkMode ? Style.dark : Style.light}>
-          <Grid container display={'flex'} flexDirection={'column'} minHeight={'100vh'}
-                justifyContent={'space-between'}>
-             <Grid item>
-                <Navigation darkMode={darkMode} handleClick={handleToggleDarkMode}/>
-             </Grid>
-             <Grid item flexGrow={1}>
-                <Routes>
-                   <Route index path={'/'} element={<Home/>} end/>
-                   <Route exact path={'/about'} element={<About/>}/>
-                   <Route exact path={'/projects/*'} element={<ProjectsPage/>}/>
-                </Routes>
-             </Grid>
-          </Grid>
-         </Box>
+  return (
+    <Box className={darkMode ? Style.dark : Style.light}>
+      <Grid
+        container
+        display={"flex"}
+        flexDirection={"column"}
+        minHeight={"100vh"}
+        justifyContent={"space-between"}
+      >
+        <Grid item>
+          <Navigation darkMode={darkMode} handleClick={handleToggleDarkMode} />
+        </Grid>
+        <Grid item flexGrow={1}>
+          <Routes>
+            <Route index path={"/"} element={<Home />} end />
+            <Route exact path={"/about"} element={<About />} />
+            <Route exact path={"/projects/*"} element={<ProjectsPage />} />
+          </Routes>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import {Box} from "@mui/material";
+import { Box } from "@mui/material";
 
-export default function ThemeSwitcher({darkMode, handleClick}) {
-    const transition = 'all 250ms ease-in-out'
+export default function ThemeSwitcher({ darkMode, handleClick }) {
+  const transition = "all 250ms ease-in-out";
 
-   return (
-      <Box fontSize={'1.5rem'} sx={{cursor: 'pointer', ":hover": {transform: 'translateY(-3px)', transition: transition}}}>
-         {
-            darkMode ?
-               <span onClick={handleClick} aria-label="Full Moon" role="img">🌕</span>
-               :
-               <span onClick={handleClick} aria-label="New Moon" role="img">🌑</span>
-         }
-      </Box>
-   )
+  return (
+    <Box
+      fontSize={"1.5rem"}
+      sx={{
+        cursor: "pointer",
+        ":hover": { transform: "translateY(-3px)", transition: transition },
+      }}
+    >
+      {darkMode ? (
+        <span onClick={handleClick} aria-label="Full Moon" role="img">
+          🌕
+        </span>
+      ) : (
+        <span onClick={handleClick} aria-label="New Moon" role="img">
+          🌑
+        </span>
+      )}
+    </Box>
+  );
 }
