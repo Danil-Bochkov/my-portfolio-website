c750f7ad67fb02378bc6a28fd71b15357c726809
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2023-02-02 18:33:18 +0200

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import { motion } from "framer-motion";
 import { data } from "../../Data/data";
 
